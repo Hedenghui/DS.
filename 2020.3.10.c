1c92f1f737e7f424eed6257963ab05bc6322056b
贺登辉 <1143671646@qq.com>
2020-03-10 21:47:39 +0800

    翻转二叉树

---
+++ 2020.3.10.c
@@ -0,0 +1,32 @@
+//·­×ªÒ»¿Ã¶þ²æÊ÷¡£
+
+#include<stdio.h>
+#include<stdlib.h>
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+     struct TreeNode *right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root)
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+
+	struct TreeNode* tmp = root->left;
+	root->left = root->right;
+	root->right = tmp;
+
+	invertTree(root->left);
+	invertTree(root->right);
+
+	return root;
+
+}
+int main()
+{
+	system("pause");
+	return 0;
+}
